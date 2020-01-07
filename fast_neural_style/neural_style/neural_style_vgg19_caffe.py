df376fbadc8d7be5211613ca88b3f069d04dd58a
Blue Nguyen <thanhcanh@acworks.co.jp>
2020-01-07 16:50:20 +0900

    Add torch jit

--- fast_neural_style/neural_style/neural_style_vgg19_caffe.py
+++ fast_neural_style/neural_style/neural_style_vgg19_caffe.py
@@ -14,7 +14,7 @@ import torch.onnx
 from PIL import Image
 
 import utils
-from transformer_net import TransformerNet
+from torch_transformer_net import TransformerNet
 from vgg import Vgg19_Caffe
 
 
@@ -225,7 +225,7 @@ def stylize(args):
         output = stylize_onnx_caffe2(content_image, args)
     else:
         with torch.no_grad():
-            style_model = TransformerNet()
+            style_model = torch.jit.script(TransformerNet())
             state_dict = torch.load(args.model)
             # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
             for k in list(state_dict.keys()):
